82a78b0ed568613d8814dc9bc22ef61f964bfb1b
vindya <vkumarostb1@gmail.com>
2019-05-20 16:31:00 +0530

    javascript first commit

---
+++ javascript/java.js
@@ -0,0 +1,5 @@
+var firstname="hello world", two ="welcome",three ="javascript";
+var lastname="hello gud afternoon"
+var one="had your launch"
+document.write(firstname +"<br>"+ two +"<br>"+ three+ "<br>");
+document.write(lastname+ ' ' +one );
